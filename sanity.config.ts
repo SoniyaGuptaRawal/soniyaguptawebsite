110d90ed400ba9e6ae8ca3ca577ed6daa5a54a14
harshilpatel22 <harshilgami26@gmail.com>
2026-02-17 14:14:08 +0000

    some changes

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,10 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./sanity/schemas";
-import { apiVersion, dataset, projectId } from "./sanity/env";
+
+const projectId = "twgqgtz0";
+const dataset = "production";
+const apiVersion = "2024-01-01";
 
 export default defineConfig({
   name: "soniya-gupta-rawal",
